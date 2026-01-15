25342745bea8034b9cde12fc6619db781f622ad8
crazy-cat-crypto <226846482+crazy-cat-crypto@users.noreply.github.com>
2026-01-15 22:05:34 +0545

    Implement robot data logging and create initial CSV file

--- src/app.py
+++ src/app.py
@@ -4,14 +4,31 @@
 # Gateway: 192.168.4.1
 
 
-
-from flask import Flask
+import csv,os
+from datetime import datetime
+from flask import Flask,request
 
 app=Flask(__name__)
 
+if not os.path.exists("robot_data.csv"):
+    with open("robot_data.csv",'w',newline="") as f:
+        writer=csv.writer(f)
+        writer.writerow(["time","distance"])
 
+
+@app.route("/",methods=["GET"])
+def save_robot_data():
+    data=request.get_json()
+    time_now=datetime.now().strftime("%H:%M:%S")
+    distance=data["distance"]
+    
+    with open("robot_data.csv","a",newline="") as f:
+        writer=csv.writer(f)
+        writer.writerow([time_now,distance])
+        
+    return 0
 
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True,host= "0.0.0.0",port=5000)
